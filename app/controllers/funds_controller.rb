67f202a2543aae504931041f1b00b2e2dde93a85
QUANG TU <quangtu2208@gmail.com>
2019-07-02 16:48:07 +0700

    User can create, update, delete capitalize

---
+++ app/controllers/funds_controller.rb
@@ -0,0 +1,47 @@
+class FundsController < ApplicationController
+  before_action :load_fund, except: %i(new create index)
+
+  def index
+    @pagy, @funds = pagy Fund.list, items: Settings.controller.funds_controller.page
+  end
+
+  def show; end
+
+  def new
+    @fund = Fund.new
+  end
+
+  def create
+    @fund = Fund.new fund_params
+    @fund.users << current_user
+    if @fund.save
+      flash[:success] = t(".created")
+      redirect_to funds_path
+    else
+      flash[:error] = t(".create_unsuccess")
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @fund.update fund_params
+      flash[:success] = t(".fund_updated")
+      redirect_to funds_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def fund_params
+    params.require(:fund).permit Fund::FUND_PARAMS
+  end
+
+  def load_fund
+    return if @fund = Fund.find_by(id: params[:id])
+    flash[:error] = t(".you_not_permited")
+    redirect_to funds_path
+  end
+end
--- app/models/fund.rb
+++ app/models/fund.rb
@@ -1,4 +1,12 @@
 class Fund < ApplicationRecord
   has_many :user_funds
   has_many :users, through: :user_funds
+
+  validates_presence_of :name
+
+  FUND_PARAMS = [:name, :total_money, :created_at, :updated_at].freeze
+
+  scope :list, (lambda do
+    select :id, :name, :total_money, :created_at, :updated_at
+  end)
 end
