e805bb84d9eaaf89c759c558a3b9ee24117f0159
anhtu268 <anhtu26896@github.com>
2019-07-02 15:57:17 +0700

    fix some bug, add toast

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,11 +6,13 @@ import 'bootstrap/dist/css/bootstrap'
 import 'mdi/css/materialdesignicons.min'
 import 'flag-icon-css/css/flag-icon.min.css'
 import 'perfect-scrollbar/css/perfect-scrollbar'
+import 'jquery-toast-plugin/dist/jquery.toast.min.css'
 
 require('jquery');
 require('popper.js');
 require('bootstrap');
 require('perfect-scrollbar');
+require('jquery-toast-plugin');
 
 ReactRailsUJS.useContext(componentRequireContext);
 window.jQuery = $;
